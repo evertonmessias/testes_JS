a7ea2e2e8011f95f4330bee2aa8386bca46b0ad1
evertonmessias <everton.messias@gmail.com>
2019-11-25 10:04:23 -0300

    25-11-2019_10:04:23

--- commitar.sh
+++ commitar.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-clear
-git add *
-echo "**************** GIT ADD OK ************************"
+git config credential.helper store
+find ./ -iname .gitignore -exec rm {} \;
+echo "##### GITIGNORE EXCLUIDO ==> OK #####"
+git add .
+echo "##### GIT ADD ==> OK #####"
 data=`date +%d-%m-%Y_%H:%M:%S`
 git commit -m $data
-#git pull
-git push --force origin master
-echo "*************** GIT COMMIT OK **********************"
+echo "##### COMMIT ==> OK #####"
+git push
+echo "##### PUSH ==> OK #####"
