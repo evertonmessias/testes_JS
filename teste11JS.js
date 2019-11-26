b49585e848fc6f8256cce2f8b69e1d240bcc053f
evertonmessias <everton.messias@gmail.com>
2019-11-26 14:23:21 -0300

    26-11-2019_14:23:21

--- teste11JS.js
+++ teste11JS.js
@@ -1,19 +1,18 @@
-// Array Multi
+// Array Multi e Associativo
+document.write("<br>Array Multi e Associativo<br>".bold().fontcolor("red"));
 
 var pessoa = new Array();
 
 pessoa[0] = ["Jose","Masc"];
 pessoa[1] = ["Maria","Fem"];
 
-alert(pessoa[1][1]);
-
-// Array Associativo 
+document.write("<br>"+pessoa[1][1]+"<br>");
 
 var pessoas = {nome:"Ana",cidade:"Campinas",grau:"Superior"};
 
-alert(pessoas["cidade"]);
+document.write("<br>"+pessoas["cidade"]+"<br>");
 
 for (var lista in pessoas){
-alert(lista + " : " + pessoas[lista]);
+document.write("<br>"+lista + " : " + pessoas[lista]);
 }
 
