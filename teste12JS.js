9432f228769eb7f726d5f71d3207f5649a4759ee
evertonmessias <everton.messias@gmail.com>
2019-11-26 15:37:53 -0300

    26-11-2019_15:37:53

--- teste12JS.js
+++ teste12JS.js
@@ -1,17 +1,17 @@
 // Objeto Date
+document.write("<br>Objeto Date<br>".bold().fontcolor("red"));
 
 var d = new Date();
 
-alert(d);
-
-alert(d.toLocaleDateString());
-alert(d.toLocaleTimeString());
+document.write("<br>data pura: "+d);
+document.write("<br>datestring: "+d.toLocaleDateString());
+document.write("<br>timestring: "+d.toLocaleTimeString());
 
 var semana = ["domingo","segunda-feira","terça-feira","quarta-feira","quinta-feira","sexta-feira","sábado"];
 var mes = new Array ("janeiro","fevefeiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro")
 
-alert("Hoje é "+ semana[d.getDay()] + ", " + d.getDate() + " de " + mes[d.getMonth()] + " de " + d.getFullYear());
+document.write("<br><br>Hoje é "+ semana[d.getDay()] + ", " + d.getDate() + " de " + mes[d.getMonth()] + " de " + d.getFullYear());
 
-alert("Em Greenwich : " + d.getUTCHours() + "hs");
+document.write("<br><br>Em Greenwich : " + d.getUTCHours() + "hs");
 
 
