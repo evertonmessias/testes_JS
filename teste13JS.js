e1d01f9e956c3c9883e3fd81a231c7a85af2d5f2
evertonmessias <everton.messias@gmail.com>
2019-11-26 15:57:02 -0300

    26-11-2019_15:57:02

--- teste13JS.js
+++ teste13JS.js
@@ -1,20 +1,23 @@
 // Classe Math
+//Atenção: Math não é um construtor - não use NEW
 
-alert("Valor de PI = " + Math.PI);
+document.write("<br>Classe Math<br>".bold().fontcolor("red"));
 
-alert("8 ^ 2 = " + Math.pow(8, 2)); 
+document.write("<br>Valor de PI = " + Math.PI);
 
-alert("Raiz de 64 = " + Math.sqrt(64));
+document.write("<br>8 ^ 2 = " + Math.pow(8, 2)); 
 
-alert("Seno de 90' = " + Math.sin(90 * Math.PI / 180));
+document.write("<br>Raiz de 64 = " + Math.sqrt(64));
 
-alert("Seno de 30' = " + Math.sin(30 * Math.PI / 180).toFixed(2));
+document.write("<br>Seno de 90' = " + Math.sin(90 * Math.PI / 180));
 
-alert("Cosseno de 0' = " + Math.cos(0 * Math.PI / 180));
+document.write("<br>Seno de 30' = " + Math.sin(30 * Math.PI / 180).toFixed(2));
 
-alert("Cosseno de 60' = " + Math.cos(60 * Math.PI / 180).toFixed(2));
+document.write("<br>Cosseno de 0' = " + Math.cos(0 * Math.PI / 180));
 
-alert("Ramdom 0 - 100 = " + (Math.random()*100).toFixed(0));
+document.write("<br>Cosseno de 60' = " + Math.cos(60 * Math.PI / 180).toFixed(2));
+
+document.write("<br>Ramdom 0 - 100 = " + (Math.random()*100).toFixed(0));
 
 
 
--- teste14JS.js
+++ teste14JS.js
@@ -1,37 +1,23 @@
 // Objeto RegExp
-
+document.write("<br>Objeto RegExp<br>".bold().fontcolor("#f00"));
 // Funções e modificadores
 
 var texto = "JavaScript é mto legal de se aprender ... ";
 
 var regex = /javascript/i; // var regex = new RegExp("JavaScript","i"); .... i = modificador para ignorar case sensitive
 
-alert(regex.test(texto));
+document.write("<br>Teste i: "+regex.test(texto));
 
 var frase = "Qual é o Doce mais dOce que o doCe ?";
 
-alert(/doce/i.exec(frase)); // traz apenas a primeira ocorrencia 
+document.write("<br>Teste i: "+  /doce/i.exec(frase)); // traz apenas a primeira ocorrencia 
 
-alert(frase.match(/doce/ig)); // função do objeto string para busca que traz todas as ocorrencias (modificador g)
+document.write("<br>Teste ig: "+  frase.match(/doce/ig)); // função do objeto string para busca que traz todas as ocorrencias (modificador g)
 
-alert(frase.replace(/doce/ig,"DOCINHO")); // substitui padrão pelo q esta nas aspas
+document.write("<br>Teste subst: "+  frase.replace(/doce/ig,"DOCINHO")); // substitui padrão pelo q esta nas aspas
 
 // Metacaracteres
 
-alert(/^j/i.test(texto));
+document.write("<br>Teste Meta: "+   /^j/i.test(texto));
 
-alert(/^\d{5}-\d{3}$/.test("13000-100")); // testa um CEP , \d para dígitos
-
-// Teste; modificar uma url:
-
-var url = "www.xti.com.br/clientes-2018.html";
-
-alert("URL antiga : " + url);
-
-var expreReg = /www.xti.com.br\/\w{2,}-\d{4}\.html/;
-
-alert("TESTA URL : " + expreReg.test(url));
-
-var Corta_expreReg = /(www.xti.com.br)\/(\w{2,})-(\d{4})\.html/; // corta com ()
-
-alert("Nova URL : " + url.replace(Corta_expreReg,"http://$1/$3/$2.php"));
+document.write("<br>Teste Cep 13000-100 : "  +   /^\d{5}-\d{3}$/.test("13000-100")); // testa um CEP , \d para dígitos
