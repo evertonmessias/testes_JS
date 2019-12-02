7915ee4d3073a034e1f9b50aa765f8526795d9c4
evertonmessias <everton.messias@gmail.com>
2019-12-02 15:32:57 -0300

    02-12-2019_15:32:57

--- teste23JS.js
+++ teste23JS.js
@@ -2,38 +2,38 @@
 
 window.onload = function () {
 
-var tit;
+    var tit;
 
-// obtendo o elemento por id, name, class, tag ou seletor css :
-
-
-tit = document.getElementById("tit_id"); // seletor id
-alert(tit);
-
-tit = document.getElementsByName("tit_name"); // seletor name
-alert(tit);
-
-tit = document.getElementsByClassName("tit_class"); // seletor Class
-alert(tit);
-
-tit = document.getElementsByTagName("h2"); // seletor tag
-alert(tit);
-
-tit = document.querySelector("#tit_id"); // seletor css
-alert(tit);
+    // obtendo o elemento por id, name, class, tag ou seletor css :
 
 
+    tit = document.getElementById("tit_id"); // seletor id
+    document.write(tit+"<br>");
 
-// depois de obter o elemento vamos muda-lo :
+    tit = document.getElementsByName("tit_name"); // seletor name
+    document.write(tit+"<br>");
+
+    tit = document.getElementsByClassName("tit_class"); // seletor Class
+    document.write(tit+"<br>");
+
+    tit = document.getElementsByTagName("h2"); // seletor tag
+    document.write(tit+"<br>");
+
+    tit = document.querySelector("#tit_id"); // seletor css
+    document.write(tit+"<br>");
 
 
-tit.innerHTML = "OUTRO TEXTO"; // mudar o texto da tag
+
+    // depois de obter o elemento vamos muda-lo :
 
 
-// mudar o estilo da tag (2 formas; por dom html ou por dom core):
+    tit.innerHTML = "OUTRO TEXTO"; // mudar o texto da tag
 
-tit.style.backgroundColor = "#0000ff"; // dom html api
 
-//tit.setAttribute("style", "color:red"); // dom core api
+    // mudar o estilo da tag (2 formas; por dom html ou por dom core):
+
+    tit.style.backgroundColor = "#0000ff"; // dom html api
+
+    //tit.setAttribute("style", "color:red"); // dom core api
 
 }
