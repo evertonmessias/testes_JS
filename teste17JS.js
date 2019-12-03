031e83b7ed4634c3d2e6740f406d9b1f261a406e
evertonmessias <everton.messias@gmail.com>
2019-12-02 21:43:27 -0300

    02-12-2019_21:43:27

--- teste17JS.js
+++ teste17JS.js
@@ -4,9 +4,9 @@
 
 
 function evento1() {
-	var resp = document.getElementById("resp").innerHTML = "BOTAO 1 (um) pressionado";
+	document.getElementById("resp").innerHTML = "BOTAO 1 (um) pressionado";
 }
 
 function evento2() {
-	var resp = document.getElementById("resp").innerHTML = "BOTAO 2 (dois) pressionado";
+	document.getElementById("resp").innerHTML = "BOTAO 2 (dois) pressionado";
 }
