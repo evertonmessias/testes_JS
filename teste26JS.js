44692736cbef69cc88dc6aae02a57ea49b0c9d73
evertonmessias <everton.messias@gmail.com>
2019-12-03 22:44:45 -0300

    03-12-2019_22:44:45

--- teste26JS.js
+++ teste26JS.js
@@ -8,8 +8,8 @@ window.onload = function () {
 	
 	form.botao.onclick = function() {
 	var tab = document.getElementsByTagName("table")[0]; // captura a tabela (está no vetor zero)
-	var row = tab.insertRow(-1);
-	var cel0 = row.insertCell(0); // deleteRow()
+	var row = tab.insertRow();
+	var cel0 = row.insertCell(0);
 	var cel1 = row.insertCell(1);
 	cel0.appendChild(document.createTextNode(form.peca.value)); // adiciona um elemento no fim
 	cel1.appendChild(document.createTextNode(form.preco.value));	
--- teste27JS.js
+++ teste27JS.js
@@ -1,38 +1,27 @@
-// DOM CORE , navegação em árvore
 
 window.onload = function () {
-	
-	var form = document.getElementById("form");
-	
-	for (var i=0;i < form.length; i++) {
-		alert(form[i] + " ==> " + i); // exibe o array do form
+
+	var botao = document.getElementById('botao');
+
+	botao.addEventListener('click', criar);
+
+	var i = 1;
+
+	function criar() {		
+
+		var form = document.getElementById("form");
+
+		var hr = document.createElement("hr");
+
+		form.appendChild(hr); // criar elemento como filho de form	
+
+		var h3 = document.createElement("h3"); // cria um h3
+
+		form.appendChild(h3);
+
+		h3.innerHTML = `Apertou o Botão ${i} vezes !!!`;h3.style.color = "red";
+
+		i++;
 	}
-	
-	
-	alert(form.parentNode); // exibe o pai, o anterior
-	
-	// CRIAR ... diferente de innerHTML que apenas modifica um existente
-	
-	var hr = document.createElement("hr");	
-	form.appendChild(hr); // criar elemento como filho de form
-	
-		
-	var h3 = document.createElement("h3"); // cria um h3  
-	form.appendChild(h3);
-	//h3.appendChild(document.createTextNode("Aperte o Botão"));
-	
-	// OU melhor em DOM HTML
-	
-	h3.innerHTML = "Aperte o Botão"
-	
-	//var atributo = document.createAttribute("style");
-	//atributo.value = "color:red";
-	//h3.setAttributeNode(atributo);
-	
-	// OU melhor em DOM HTML
-	
-	h3.style.color = "red"; 
-	
-	
-	
+
 }
