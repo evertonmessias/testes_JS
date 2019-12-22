919596d0de5aa3d6d38172cbec23537fd5d83b95
evertonmessias <everton.messias@gmail.com>
2019-12-22 19:30:14 -0300

    22-12-2019_19:30:14

--- teste11JS.js
+++ teste11JS.js
@@ -1,21 +1,85 @@
-var dados = {
-    nome : "Everton",    
-    idade : 43,
-    peso: 102,
-    engordar(p){
-        console.log("Engordou!");
-        this.peso += p;
-    }	
-};
+window.onload = function () {
+    var mais = document.getElementById("mais");
+    var maiss = document.getElementById("maiss");
 
-for (var lista in dados){
-console.log(lista +" : "+ dados[lista]);
+    var naturalidade = "Santos";
+
+    var dados = {
+        nome: "Everton",
+        idade: 43,
+        naturalidade,
+        fisico: { peso: 102, altura: 1.77, cor: 'moreno' },
+        engordar(p) {
+            console.log("Engordou!");
+            this.fisico.peso += p;
+        }
+    };
+
+    var profissao = {
+        linguagens: ['js', 'php', 'c', 'script'],
+        novalinguagem: function (nova) {
+            console.log("Nova Linguagem");
+            this.linguagens[4] = nova;
+
+        }
+    };
+
+    console.log(dados);
+    console.log(profissao);
+
+    mais.onclick = function () {
+
+        dados.engordar(8);
+
+        profissao.novalinguagem('nodejs');
+
+        //delete dados.idade;
+
+        dados.datanasc = '25/05/1975';
+        nomeVariavel = "objetivo";
+
+        var novoObjeto = {
+            ...dados, // ... espalha no novoObjeto
+            ...profissao,
+            [nomeVariavel]: "Analista", // ECMA 6
+            nivel: "Superior"
+        };
+
+        //var novoObjeto = Object.assign({},dados,profissao);    
+        //Object.assign(dados,profissao); //(Obs: este modifica dados)
+
+        console.log(novoObjeto);
+
+    }
+    maiss.onclick = function () {
+        var pessoas = [ // array de objetos
+            { nome: 'Dhora', idade: 47 },
+            { nome: 'Erika', idade: 23 },
+            { nome: 'Clelia', idade: 65 }
+        ];      
+
+        var busca = {
+            posicao: 0,
+            get atual() {
+                return pessoas[this.posicao]
+            },
+            set atual(posicao){
+                this.posicao = posicao;
+            },
+            proximo() {
+                ++this.posicao;
+            },
+            anterior() {
+                --this.posicao;
+            }
+        }
+        console.log(busca.atual);
+        busca.proximo();
+        console.log(busca.atual);
+        busca.atual = 2;
+        console.log(busca.atual);
+    }
+
 }
-
-console.log(`Nome: ${dados.nome}, Idade: ${dados.idade}, Peso: ${dados.peso}`);
-dados.engordar(8);
-console.log(`Nome: ${dados.nome}, Idade: ${dados.idade}, Peso: ${dados.peso}`);
-
-
 
 
---
+++ teste21JS.js
@@ -0,0 +1,35 @@
+window.onload = function () {
+
+    var botao = document.getElementById("botao");
+    var imc = document.getElementById("imc");
+    var resp = document.getElementById("cond");
+
+    botao.onclick = function () {
+
+        var peso = parseFloat(document.getElementById("peso").value);
+        var altura = parseFloat(document.getElementById("altura").value);
+
+        var dados = {
+            peso, altura,
+            calcIMC() {
+                if ((this.peso >= 20 && this.peso <= 200) && (this.altura >= 1 && this.altura <= 2.5)) {
+                    return ( this.peso / (Math.pow(this.altura, 2))).toFixed(0);
+                } else { return "Dados incorretos!" }
+            },
+            condicao() {
+                if (this.calcIMC() <= 17) { return "Muito abaixo"; }
+                else if (this.calcIMC() > 17 && this.calcIMC() <= 19) { return "Abaixo do peso"; }
+                else if (this.calcIMC() > 19 && this.calcIMC() <= 25) { return "Peso Normal"; }
+                else if (this.calcIMC() > 25 && this.calcIMC() <= 30) { return "Acima do peso"; }
+                else if (this.calcIMC() > 30 && this.calcIMC() <= 35) { return "Obesidade Leve"; }
+                else if (this.calcIMC() > 35 && this.calcIMC() <= 40) { return "Obesidade Severa"; }
+                else if (this.calcIMC() > 40) { return "Obesidade Mórbida"; }
+                else {return "Erro";}
+            }                       
+        };
+        console.log(peso,altura);              
+
+        imc.innerHTML = `IMC = ${dados.calcIMC()}`;
+        resp.innerHTML = dados.condicao();
+    }
+}
