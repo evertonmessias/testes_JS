dfeb5fe6e331a23a814c12a627023f6537117953
evertonmessias <everton.messias@gmail.com>
2019-12-24 10:35:56 -0300

    24-12-2019_10:35:56

--- teste21JS.js
+++ teste21JS.js
@@ -1,16 +1,16 @@
 window.onload = function () {
 
-    document.getElementById("botao").onclick = function () {       
+    document.getElementById("botao").onclick = function () {
 
-        var dados = function(){ // pode ser uma classe ...
-            this.peso = Number(document.getElementById("peso").value);
-            this.altura = Number(document.getElementById("altura").value);
-            this.calcIMC = function() {
+        var dados = function (p, a) { // pode ser uma classe ...
+            this.peso = p;
+            this.altura = a;
+            this.calcIMC = () => {
                 if ((this.peso >= 20 && this.peso <= 200) && (this.altura >= 1 && this.altura <= 2.5)) {
-                    return ( this.peso / (Math.pow(this.altura, 2))).toFixed(0);
+                    return (this.peso / (Math.pow(this.altura, 2))).toFixed(0);
                 } else { return "Dados incorretos!" }
             }
-            this.condicao = function() {
+            this.condicao = () => {
                 if (this.calcIMC() <= 17) { return "Muito abaixo"; }
                 else if (this.calcIMC() > 17 && this.calcIMC() <= 19) { return "Abaixo do peso"; }
                 else if (this.calcIMC() > 19 && this.calcIMC() <= 25) { return "Peso Normal"; }
@@ -18,12 +18,12 @@ window.onload = function () {
                 else if (this.calcIMC() > 30 && this.calcIMC() <= 35) { return "Obesidade Leve"; }
                 else if (this.calcIMC() > 35 && this.calcIMC() <= 40) { return "Obesidade Severa"; }
                 else if (this.calcIMC() > 40) { return "Obesidade Mórbida"; }
-                else {return "Erro";}
-            }                       
+                else { return "Erro"; }
+            }
         }
-        var calculo = new dados();
+        var calculo = new dados(Number(document.getElementById("peso").value), Number(document.getElementById("altura").value));
         document.getElementById("imc").innerHTML = `IMC = ${calculo.calcIMC()}`;
-        document.getElementById("cond").innerHTML = calculo.condicao();        
-        console.log(calculo.peso,calculo.altura); 
+        document.getElementById("cond").innerHTML = calculo.condicao();
+        console.log(calculo.peso, calculo.altura);
     }
 }
