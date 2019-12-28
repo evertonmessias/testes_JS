04e04df53b9f383ae31ba3016a16b721e0e135d9
evertonmessias <everton.messias@gmail.com>
2019-12-27 22:55:48 -0300

    27-12-2019_22:55:48

--- teste26JS.js
+++ teste26JS.js
@@ -1,16 +1,26 @@
 // DOM HTML
 
-// uma forma de inserir dados na tabela ....
+// inserir dados em uma tabela ....
 
 window.onload = function () {
 	
 	var form = document.getElementById("form");
 	
 	form.botao.onclick = function() {
-	var tab = document.getElementsByTagName("table")[0]; // captura a tabela (está no vetor zero)
+	var tab = document.getElementById("tabela");
+	var row = document.createElement('tr');
+	var cel0 = document.createElement('td');
+	var cel1 = document.createElement('td');
+	tab.append(row);
+	row.append(cel0);
+	row.append(cel1);
+
+	/*
 	var row = tab.insertRow();
 	var cel0 = row.insertCell(0);
 	var cel1 = row.insertCell(1);
-	cel0.appendChild(document.createTextNode(form.peca.value)); // adiciona um elemento no fim
-	cel1.appendChild(document.createTextNode(form.preco.value));	
+	*/
+
+	cel0.append(form.peca.value);
+	cel1.append(form.preco.value);
 }}
--- teste27JS.js
+++ teste27JS.js
@@ -1,27 +1,13 @@
-
 window.onload = function () {
 
-	var botao = document.getElementById('botao');
+    for (var i = 0; i <= 2; i++) {
+        `var ex${i} = document.getElementById("ex${i}")`;
+    }
 
-	botao.addEventListener('click', criar);
-
-	var i = 1;
-
-	function criar() {		
-
-		var form = document.getElementById("form");
-
-		var hr = document.createElement("hr");
-
-		form.appendChild(hr); // criar elemento como filho de form	
-
-		var h3 = document.createElement("h3"); // cria um h3
-
-		form.appendChild(h3);
-
-		h3.innerHTML = `Apertou o Botão ${i} vezes !!!`;h3.style.color = "red";
-
-		i++;
-	}
-
+    ex0.onclick = function () {
+        window.location.reload();
+    }
+    ex1.onclick = function () { 
+        console.log("--------------------");
+    }
 }
