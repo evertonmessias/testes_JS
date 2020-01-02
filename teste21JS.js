218122b333dcc1b73f1fd622e62defe23c73a0d4
evertonmessias <everton.messias@gmail.com>
2020-01-01 23:55:35 -0300

    01-01-2020_23:55:35

--- teste21JS.js
+++ teste21JS.js
@@ -1,28 +1,42 @@
-// AJAX + DOM HTML
-// inserir dados em uma tabela e depois escrever num arquivo ....
-
 window.onload = function () {
-
     var form = document.getElementById("form");
+    var tab = document.getElementById("tabela");
 
-    form.botao.onclick = function () {
-        if (form.peca.value && form.preco.value) {
-            var peca = form.peca.value;
-            var preco = form.preco.value;
-            var tab = document.getElementById("tabela");
-            var row = document.createElement('tr');
-            var cel0 = document.createElement('td');
-            var cel1 = document.createElement('td');
-            tab.append(row);
-            row.append(cel0);
-            row.append(cel1);
-            cel0.append(peca);
-            cel1.append(preco);
-            var texto = { Peça: peca, Preço: preco }
+    var db = openDatabase('teste21', '1.0', 'Exemplo', 2 * 1024 * 1024);
+    
+    db.transaction(function (resp) {
+        resp.executeSql("CREATE TABLE IF NOT EXISTS agenda (id INTEGER PRIMARY KEY, nome TEXT, email TEXT)");
+    });
+
+    db.transaction(function (resp) {
+        resp.executeSql('SELECT * from agenda', [], function (resp, resultados) {
+            var rows = resultados.rows;
+            for (var i = 0; i < rows.length; i++) {                
+                var row = document.createElement('tr');
+                var cel0 = document.createElement('td');
+                var cel1 = document.createElement('td');
+                var cel2 = document.createElement('td');                
+                tab.append(row);
+                row.append(cel0);
+                row.append(cel1);
+                row.append(cel2);                
+                cel0.append(i+1);
+                cel1.append(rows[i].nome);
+                cel2.append(rows[i].email);
+            }
+        });
+    });
+    form.inserir.onclick = function () {
+        if (form.nome.value && form.email.value) {
+            var nome = form.nome.value;
+            var email = form.email.value;
+            db.transaction(function (resp) {
+                resp.executeSql('INSERT INTO agenda (nome, email) VALUES(?,?)', [nome, email]);
+            });
+            var texto = { Nome: nome, EMail: email }
             console.log("OK ==>", texto);
-
-            // salvar em arquivo:
-            var arquivo = "./testejson.json";}
-
+            window.location.reload();
+        }
     }
-}		
+
+}	
