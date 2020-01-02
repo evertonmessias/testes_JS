545a987c9b9efe52a3af3981de21532e6fe61d9a
evertonmessias <everton.messias@gmail.com>
2020-01-02 17:17:50 -0300

    02-01-2020_17:17:50

--- teste21JS.js
+++ teste21JS.js
@@ -1,34 +1,38 @@
+var db = openDatabase('teste21', '1.0', 'Exemplo', 2 * 1024 * 1024);
+
+db.transaction(function (resp) {
+    resp.executeSql("CREATE TABLE IF NOT EXISTS agenda (id INTEGER PRIMARY KEY, nome TEXT, email TEXT)");
+});
+
+function reload() {
+    window.location.reload();
+}
+
+function apagar(n) {
+    db.transaction(function (resp) {
+        resp.executeSql("DELETE FROM agenda WHERE id = ?",[n]);
+    });
+    setTimeout(reload, 500);
+}
+
 window.onload = function () {
+
     var form = document.getElementById("form");
     var tab = document.getElementById("tabela");
 
-    function apagar(n){
-        console.log("APAGA",n);
-    }
-
-    var db = openDatabase('teste21', '1.0', 'Exemplo', 2 * 1024 * 1024);
-    
-    db.transaction(function (resp) {
-        resp.executeSql("CREATE TABLE IF NOT EXISTS agenda (id INTEGER PRIMARY KEY, nome TEXT, email TEXT)");
-    });
-
     db.transaction(function (resp) {
         resp.executeSql('SELECT * from agenda', [], function (resp, resultados) {
-            var rows = resultados.rows;var tr = '';
-            for (var i = 0; i < rows.length; i++) {                
-                tr += '<tr onClick="apagar('+rows[i].id+')" id="linha">';
-                tr += "<td>"+rows[i].id+'</td>';
-                tr += '<td>'+rows[i].nome+'</td>';
-                tr += '<td>'+rows[i].email+'</td>';
-                tr += '</tr>'                
+            var rows = resultados.rows; var tr = '';
+            for (var i = 0; i < rows.length; i++) {
+                tr += '<tr onClick="apagar(' + rows[i].id + ')" id="linha">';
+                tr += "<td>" + rows[i].id + '</td>';
+                tr += '<td>' + rows[i].nome + '</td>';
+                tr += '<td>' + rows[i].email + '</td>';
+                tr += '</tr>'
             }
             tab.innerHTML = tr;
         });
     });
-
-    function reload(){
-        window.location.reload();
-    }
 
     form.inserir.onclick = function () {
         if (form.nome.value && form.email.value) {
@@ -38,9 +42,8 @@ window.onload = function () {
                 resp.executeSql('INSERT INTO agenda (nome, email) VALUES(?,?)', [nome, email]);
             });
             var texto = { Nome: nome, EMail: email }
-            console.log("OK ==>", texto); 
-            setTimeout(reload,500);           
+            console.log("OK ==>", texto);
+            setTimeout(reload, 500);
         }
     }
-
-}	
+}
