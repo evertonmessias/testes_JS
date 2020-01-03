4cd5c3101f0f59e78e972a5b7010b0a6ba7fd937
evertonmessias <everton.messias@gmail.com>
2020-01-03 17:09:27 -0300

    03-01-2020_17:09:27

--- teste30JS.js
+++ teste30JS.js
@@ -2,11 +2,11 @@
 
 // https://viacep.com.br/ws/13101014/json
 
-var cep = '13101014';
+var cep = '13101014'
 
-window.fetch('https://viacep.com.br/ws/'+cep+'/json')
+window.fetch('https://viacep.com.br/ws/' + cep + '/json')
     .then((data) =>
         data.json()
-            .then((endereco) =>console.log(endereco))
-            .catch(()=>console.log("Erro"))
-            )
+            .then((endereco) => console.log(endereco))
+            .catch(() => console.log("Erro"))
+    )
