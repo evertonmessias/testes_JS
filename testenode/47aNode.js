6e89afcd415289e2c5bee4e574cb2bd12ffbaa15
evertonmessias <everton.messias@gmail.com>
2020-01-13 10:09:53 -0300

    13-01-2020_10:09:53

--- testenode/47aNode.js
+++
@@ -1,3 +0,0 @@
-var calcIMC = require("./47bNode"); 
-var calc = calcIMC(105,1.77);
-console.log("IMC: "+calc.valor," ; Condicao: "+calc.condicao);
--- testenode/48Node.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var porta = 8080;
-
-http.createServer(function(req,res){
-    res.end("<h1>Ola</h1>");
-}).listen(porta);
-
-console.log('O servidor está On-Line na porta '+porta);
